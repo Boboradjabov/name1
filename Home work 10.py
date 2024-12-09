d68f550928feb1f8eab1d2febadcc5c3ac1c903e
Boboradjabov <yasmin140898@gmail.com>
2024-12-09 18:33:09 +0500

    2

--- Home work 10.py
+++ Home work 10.py
@@ -1,26 +1,12 @@
-class Animal:
-  def make_sound(self):
-        print('Эти животные издают звук')
-  def rr(self):
-      print('Кот муркает')
-  def gav(self):
-      print('собака гавкает')
-  def muu(self):
-      print('корова мычит')
-animal1=Animal()
-animal1.make_sound()
+class Animal():
+    def __init__(self,sound=''):
+        self.sound = sound
+
+    def make_sound(self):
+        print(f'{self.sound}')
+
 class Cat(Animal):
-    def __init__(self,a):
-        a=a
-cat1=Cat
-cat1.rr(animal1)
-class Dog(Animal):
-    def __init__(self,b):
-        b=b
-dag=Dog
-dag.gav(animal1)
-class Cow(Animal):
-    def __init__(self,c):
-        c=c
-caw=Cow
-caw.muu(animal1)
+    def __init__(self,sound='may'):
+        super().__init__(sound)
+
+Cat().make_sound()
